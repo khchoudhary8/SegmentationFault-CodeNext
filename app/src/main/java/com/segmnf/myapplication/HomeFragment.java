7a4d082999ef8bb606554e833c97ddc5c3995ed4
khcho <khchoudhary8@gmail.com>
2022-07-19 17:15:05 +0530

    initial commit

---
+++ app/src/main/java/com/segmnf/myapplication/HomeFragment.java
@@ -0,0 +1,70 @@
+package com.segmnf.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class HomeFragment extends Fragment {
+
+    private FirebaseDatabase database;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        database = FirebaseDatabase.getInstance("https://tynkr-3915c-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView name = view.findViewById(R.id.username);
+        ImageView pp = view.findViewById(R.id.pp);
+        ImageView news= view.findViewById(R.id.imageView4);
+
+        database.getReference().child("User_details").child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            UserModel model = snapshot.getValue(UserModel.class);
+                            name.setText(model.getName());
+                            Glide.with(getActivity()).load(model.getImage()).into(pp);
+                        }
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            news.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getContext(), NewsActivity.class);
+                    startActivity(intent);
+                }
+            });
+        return view;
+    }
+}
